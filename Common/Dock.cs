d799c30f0db5b89ccc31ea480aa719bf36f8d793
birbilis <birbilis@localhost>
2015-01-23 23:24:01 +0200

    - first version

---
+++ Common/Dock.cs
@@ -0,0 +1,30 @@
+﻿//based on http://silverlight.codeplex.com/SourceControl/latest#Release/Silverlight4/Source/Controls.Toolkit/DockPanel/DockPanel.cs
+
+// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+namespace System.Windows.Controls
+{
+  public enum Dock
+
+  {
+    // Summary:
+    //     Specifies that the control should be positioned on the left of the control.
+    Left = 0,
+    //
+    // Summary:
+    //     Specifies that the control should be positioned on top of the control.
+    Top = 1,
+    //
+    // Summary:
+    //     Specifies that the control should be positioned on the right of the control.
+    Right = 2,
+    //
+    // Summary:
+    //     Specifies that the control should be positioned at the bottom of the control.
+    Bottom = 3,
+  }
+
+}
